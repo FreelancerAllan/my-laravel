db2e643eccbc4909cda5bb79b523a142a7eba126
FreelancerAllan <rutoallan2017@gmail.com>
2025-01-03 08:22:11 +0300

    learning continues

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+return view('welcome');
+    });
+
+
+
+
+    //http://127.0.0.1:8000/about
+    //naming route
+Route::get('/about', function () {
+    return "This is a home page";
+})->name('about');
+
+
+//routes with parameters
+Route::get('/user/{id}/{slug}', function ($id,$slug) {
+    return "Hello"  .$id.'-'.$slug;
+})->name('user');
