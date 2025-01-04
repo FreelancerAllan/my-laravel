8c6be4765fd843e9f1cd88b9db1f2a8727d57545
FreelancerAllan <rutoallan2017@gmail.com>
2025-01-04 08:01:37 +0300

    saturday

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-return view('welcome');
-    });
+     return view('index');
+         });
+        
+        
+        
+        
+            //http://127.0.0.1:8000/about
+         //naming route
+     Route::get('/about', function () {
+         return "This is a home page";
+     })->name('about');
+        
+        
+     //routes with parameters
+     Route::get('/user/{id}/{slug}', function ($id,$slug) {
+            return "Hello"  .$id.'-'.$slug;
+        })->name('user');
 
 
 
+//grouping routes
+//http://127.0.0.1:8000/blog/create
+//http://127.0.0.1:8000/blog/edit
+//http://127.0.0.1:8000/blog/show
+Route::group(['prefix'=>'blog'],   function(){
+    Route::get('/create', function () {
+        return "Blog Create Page";
+            })->name('create');
 
-    //http://127.0.0.1:8000/about
-    //naming route
-Route::get('/about', function () {
-    return "This is a home page";
-})->name('about');
+            Route::get('/edit', function () {
+                return "Blog edit Page";
+                    })->name('edit');
 
+                    Route::get('/show', function () {
+                        return "Blog show Page";
+                            })->name('show');
 
-//routes with parameters
-Route::get('/user/{id}/{slug}', function ($id,$slug) {
-    return "Hello"  .$id.'-'.$slug;
-})->name('user');
+});
